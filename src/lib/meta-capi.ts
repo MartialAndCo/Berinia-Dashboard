9421cad342152b5419d1b80bc7cb285e8df7a6a0
martialandco <yannrosemark@gmail.com>
2026-09-17 00:18:38 +0200

    fix: remove pixel 2349908955753012, enforce business portfolio pixel 1118345583879065 exclusively

--- src/lib/meta-capi.ts
+++ src/lib/meta-capi.ts
@@ -49,7 +49,7 @@ export async function sendMetaCapiEvent(
   const pixelId =
     process.env.META_PIXEL_ID ||
     process.env.NEXT_PUBLIC_META_PIXEL_ID ||
-    "2349908955753012";
+    "1118345583879065";
   const accessToken = process.env.META_CONVERSIONS_API_TOKEN;
   const testEventCode = process.env.META_TEST_EVENT_CODE;
 
